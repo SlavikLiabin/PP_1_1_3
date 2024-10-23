903bb055d8bdd6c2a66dbe3f63c4b86611bbe175
Viacheslav Liabin <R9514633542@gmail.com>
2024-10-23 15:00:28 +0500

    Added logic to Main and Util files

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Петр", "Петров", (byte) 28);
+        userService.saveUser("Иван", "Иванов", (byte) 32);
+        userService.saveUser("Сергей", "Сергеев", (byte) 48);
+        userService.saveUser("Андрей", "Андреев", (byte) 53);
+
+        System.out.println("Список всех пользователей:");
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,63 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+    private static Util instance = null;
+    private static final Logger logger = Logger.getLogger(Util.class.getName());
+
+    private Util() {
+        try {
+            Properties props = getProps();
+            connection = DriverManager.getConnection(
+                    props.getProperty("db.url"),
+                    props.getProperty("db.username"),
+                    props.getProperty("db.password")
+            );
+        } catch (SQLException | IOException e) {
+            logger.log(Level.SEVERE, "Database connection error", e);
+        }
+    }
+
+    public static synchronized Util getInstance() {
+        if (instance == null) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                logger.log(Level.SEVERE, "Error closing connection", e);
+            }
+        }
+    }
+
+    private static Properties getProps() throws IOException {
+        Properties props = new Properties();
+        try (InputStream in = Files.newInputStream(Paths.get(Util.class.getResource("/database.properties").toURI()))) {
+            props.load(in);
+        } catch (IOException | URISyntaxException e) {
+            throw new IOException("Database config file not found", e);
+        }
+        return props;
+    }
 }
